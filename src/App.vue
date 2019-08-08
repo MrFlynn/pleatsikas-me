e46983bb382846780b2566132280ca6ca6f32572
MrFlynn <nick@pleatsikas.me>
2019-08-08 00:14:47 -0700

    Added basic format for title region.
    Added contact window popup button. Contact form shows up and works.

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,44 @@
   <div id="app">
     <div class="columns is-gapless">
       <div class ="column is-one-quarter has-background-light">
-        <h1>Hello world</h1>
+        <article class="media">
+          <div class="media-left">
+            <figure class="image is-128x128">
+              <img class="is-rounded" src="./assets/profile.jpg">
+            </figure>
+          </div>
+          <div class="media-content">
+            <div class="content">
+              <h1 class="title">Nick Pleatsikas</h1>
+              <h3 class="subtitle">Software Engineer</h3>
+              <button @click="showModal()" class="button is-info">Contact Me</button>
+            </div>
+          </div>
+        </article>
       </div>
       <div class="column">
         <h1>This is the content region</h1>
       </div>
     </div>
+    <ContactModal/>
   </div>
 </template>
 
 <script>
+import {eventBus} from './main';
+
+import ContactModal from './components/ContactModal.vue'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    ContactModal
+  },
+  methods: {
+    showModal() {
+      eventBus.$emit('modalHidden', false)
+    }
+  }
 }
 </script>
 
@@ -23,7 +49,6 @@ export default {
 
 <style>
   .columns {
-    display: flex;
     height: 100vh;
   }
 </style>
