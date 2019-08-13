09edfd8ad85670ed021ec1d3dee1cb19112b9e6b
MrFlynn <nick@pleatsikas.me>
2019-08-12 22:37:59 -0700

    Updated "About me" section.

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <h1 class="title is-3">About me</h1>
+    <div class="content">
+      <p>
+        Hello there! I'm Nick, a computer engineering student at the 
+          <a class="underline" href="https://ucr.edu">University of California, Riverside</a>.
+        My focus is on studying high performance computing and networking. My senior year will
+        be spent taking design level courses in computer networking, concurrent and parallel 
+        systems, and GPU programming.
+      </p>
+      <p>
+        I'm also a part-time infrastructure engineer/DevOps at a ML/DL company called
+          <a class="underline" href="https://minds.ai">Minds.ai</a>.
+        I have experience in designing and writing production-grade software in Python. I have
+        also created developer tools that simplify tasks for other engineers and IT staff.
+      </p>
+      <p>
+        Outside of work and studying I'm a member of my University's ACM branch. I'm 
+        also an avid soccer player, gym goer, and foodie.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutMe'
+}
+</script>
