22f63b11d2e011b36b6ff6acf1f588ed6f9124de
MrFlynn <nick@pleatsikas.me>
2019-08-15 00:36:07 -0700

    Finished section on work experience.

--- src/components/WorkExperience.vue
+++ src/components/WorkExperience.vue
@@ -2,16 +2,19 @@
   <div class="container">
     <h1 class="title is-3">Work experience</h1>
     <div class="content">
-      <div v-for="(job, idx) in jobs" :key="idx">
+      <div v-for="(job, i) in jobs" :key="i">
         <div class="job-entry">
           <p>
-            <span class="tag is-medium is-primary">{{ job.start }} - {{ job.end }}</span>
-            <span class="tag is-medium is-dark">{{ job.position }}</span>
             <span class="tag is-medium is-info link">
               <a :href=job.company.url>{{ job.company.name }}</a>
             </span> 
+            <span class="tag is-medium is-dark">{{ job.position }}</span>
+            <span class="tag is-medium is-light">{{ job.start }} - {{ job.end }}</span>
           </p>
-          <p><strong>Description:</strong> {{ job.description }}</p>
+          <strong>Description:</strong>
+          <ul>
+            <li v-for="(line, j) in job.description" :key="j">{{ line }}</li>
+          </ul>
         </div>
       </div>
     </div>
@@ -32,10 +35,11 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+  $blue: #0E7C7B
   @import "bulma/sass/utilities/initial-variables.sass"
 
   .job-entry
-    padding-bottom: 1rem
+    padding-bottom: 1.5rem
 
     span
       margin-right: 0.4rem
@@ -47,6 +51,9 @@ export default {
 
     p
       margin-bottom: 0 !important
+
+    ul
+      margin-top: 0
 
   .link
     &:hover
